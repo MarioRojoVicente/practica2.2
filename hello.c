063de3cf0890ed4621e8b0eb007e2a79f404e503
MarioRojoVicente <79108855+MarioRojoVicente@users.noreply.github.com>
2021-03-18 19:27:48 +0100

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+/* ----------------------------------------------------------------------- */
+/*                (C) Mario Rojo Vicente [bp0477]                 */
+/* ----------------------------------------------------------------------- */
+
+#include <stdio.h>                                      /* printf, getchar */
+
+int main ( void )
+{
+    printf("\n Hola MINIX3 de parte de Mario Rojo Vicente") ;
+    getchar() ;
+	putchar('\n') ;
+    return 0 ;
+}
